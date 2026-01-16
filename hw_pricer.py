b32cbe36060be5f49644fc8b25ee77a97b13c0bb
Krupam Patel <x16pat@gmail.com>
2026-01-15 22:09:09 -0600

    Update hw_pricer.py

--- hw_pricer.py
+++ hw_pricer.py
@@ -1,5 +1,4 @@
 """Interest Rate Derivatives Pricing"""
-#FULLY DONE
 
 import numpy as np
 from scipy.stats import norm
@@ -26,12 +25,12 @@ class hwPricer:
             bond_price = self.model.discount(bond_maturity)
             return max(k - bond_price, 0)
 
-        if mc:
+        if mc: # this is monte carlo simulation btw 
             df = self.model.discount(option_expiry)
             bond_price_sim = self.curve_sim.zero_coupon_bond(option_expiry, bond_maturity, fwd_measure=True)
             payoff = np.maximum(k - bond_price_sim, 0)
             option_value = np.mean(df * payoff)
-        else:
+        else: #analytical pricing
             sigma = self.model.parameters['sigma']
             a = self.model.parameters['a']
             bond_sens = self.model.rate_sens(option_expiry, bond_maturity)
@@ -61,7 +60,7 @@ class hwPricer:
 
         return option_value
 
-    def caplet(self, fixing_time, payment_time, notional, k, method='js'):
+    def caplet(self, fixing_time, payment_time, N, k, method='js'):
         accrual_period = payment_time - fixing_time
         k_bond = 1 + k * accrual_period
 
@@ -85,9 +84,9 @@ class hwPricer:
             h = (1 / vol_bond) * np.log(discount_payment * k_bond / discount_fixing) + 0.5 * vol_bond
             caplet_value = discount_fixing * norm.cdf(-h + vol_bond) - k_bond * discount_payment * norm.cdf(-h)
 
-        return notional * caplet_value
+        return N * caplet_value
 
-    def cap(self, payment_schedule, notional, k, mc=False):
+    def cap(self, payment_schedule, N, k, mc=False):
         cap_value = 0
         if mc:
             for i in range(1, len(payment_schedule)):
@@ -109,9 +108,9 @@ class hwPricer:
                 put_price = self.zero_bond_put(fixing_time, payment_time, 1 / k_bond)
                 cap_value += k_bond * put_price
 
-        return notional * cap_value
+        return N * cap_value
 
-    def floor(self, payment_schedule, notional, k, mc=False):
+    def floor(self, payment_schedule, N, k, mc=False):
         floor_value = 0
         if mc:
             for i in range(1, len(payment_schedule)):
@@ -131,9 +130,9 @@ class hwPricer:
                 call_price = self.zero_bond_call(fixing_time, payment_time, 1 / k_bond)
                 floor_value += k_bond * call_price
 
-        return notional * floor_value
+        return N * floor_value
 
-    def swap(self, payment_schedule, notional, fixed_rate, payer=True, mc=False):
+    def swap(self, payment_schedule, N, fixed_rate, payer=True, mc=False):
         direction = 1 if payer else -1
         annuity = 0
         for i in range(1, len(payment_schedule)):
@@ -155,10 +154,10 @@ class hwPricer:
         else:
             floating_leg_pv = self.model.discount(payment_schedule[0]) - self.model.discount(payment_schedule[-1])
 
-        swap_value = notional * direction * (floating_leg_pv - fixed_leg_pv)
+        swap_value = N * direction * (floating_leg_pv - fixed_leg_pv)
         return swap_value
 
-    def swaption(self, payment_schedule, notional, fixed_rate, payer=True, mc=False):
+    def swaption(self, payment_schedule, N, fixed_rate, payer=True, mc=False):
         direction = 1 if payer else -1
         option_expiry = payment_schedule[0]
         swap_maturity = payment_schedule[-1]
@@ -184,7 +183,7 @@ class hwPricer:
                 fixed_leg_value += accrual_period * fixed_rate * bond_price_i
 
             disc_expiry = self.model.discount(option_expiry)
-            swaption_value = disc_expiry * notional * np.mean(np.maximum(direction * (floating_leg_value - fixed_leg_value), 0))
+            swaption_value = disc_expiry * N * np.mean(np.maximum(direction * (floating_leg_value - fixed_leg_value), 0))
 
         else:
             critical_rate = self.find_rstar(option_expiry, payment_schedule, fixed_rate)
@@ -203,11 +202,11 @@ class hwPricer:
             bond_adj_final = self.model.bond_adj_factor(option_expiry, swap_maturity)
             strike_final = bond_adj_final * np.exp(-bond_sens_final * critical_rate)
             floating_leg_value = self.zero_bond_put(option_expiry, swap_maturity, strike_final) if payer else self.zero_bond_call(option_expiry, swap_maturity, strike_final)
-            swaption_value = notional * (floating_leg_value + fixed_leg_value)
+            swaption_value = N * (floating_leg_value + fixed_leg_value)
 
         return swaption_value
 
-    def coupon_bond(self, payment_schedule, coupon_rate, notional):
+    def coupon_bond(self, payment_schedule, coupon_rate, N):
         bond_price = 0
         for i in range(len(payment_schedule)):
             curr_time = payment_schedule[i]
@@ -215,31 +214,31 @@ class hwPricer:
             accrual_period = curr_time - previous_time
 
             disc_payment = self.curve.discount(curr_time)
-            coupon_cashflow = notional * coupon_rate * accrual_period
+            coupon_cashflow = N * coupon_rate * accrual_period
 
             if i == len(payment_schedule) - 1:
-                coupon_cashflow += notional
+                coupon_cashflow += N
 
             bond_price += coupon_cashflow * disc_payment
 
         return bond_price
 
-    def floating_rate_note(self, payment_schedule, notional):
-        disced_coupons = self.swap(payment_schedule, notional, fixed_rate=0, payer=False, mc=False)
-        disced_notional = notional * self.model.discount(payment_schedule[-1])
+    def floating_rate_note(self, payment_schedule, N):
+        disced_coupons = self.swap(payment_schedule, N, fixed_rate=0, payer=False, mc=False)
+        disced_notional = N * self.model.discount(payment_schedule[-1])
         frn_price = disced_coupons + disced_notional
         return frn_price
 
-    def bond_option(self, option_expiry, payment_schedule, coupon_rate, k, notional, call=True, mc=False):
+    def bond_option(self, option_expiry, payment_schedule, coupon_rate, k, N, call=True, mc=False):
         if mc:
-            bond_price_dist = self.curve_sim.coupon_bond(option_expiry, payment_schedule, coupon_rate, notional)
+            bond_price_dist = self.curve_sim.coupon_bond(option_expiry, payment_schedule, coupon_rate, N)
             disc_factor = self.model.discount(option_expiry)
             option_value = disc_factor * np.mean(
                 np.maximum(bond_price_dist - k, 0) if call else np.maximum(k - bond_price_dist, 0)
             )
 
         else:
-            critical_rate = self.find_rstar_bond(option_expiry, payment_schedule, coupon_rate, notional, k)
+            critical_rate = self.find_rstar_bond(option_expiry, payment_schedule, coupon_rate, N, k)
             option_value = 0
 
             for i in range(len(payment_schedule)):
@@ -253,9 +252,9 @@ class hwPricer:
 
                 option = self.zero_bond_call(option_expiry, curr_time, k_bond) if call else self.zero_bond_put(option_expiry, curr_time, k_bond)
 
-                coupon_cashflow = notional * coupon_rate * accrual_period
+                coupon_cashflow = N * coupon_rate * accrual_period
                 if i == len(payment_schedule) - 1:
-                    coupon_cashflow += notional
+                    coupon_cashflow += N
 
                 option_value += coupon_cashflow * option
 
@@ -286,7 +285,7 @@ class hwPricer:
             critical_rate = brentq(root_func, -10.0, 10.0, xtol=1e-12)
         return critical_rate
 
-    def jams_root_bond(self, option_expiry, payment_schedule, coupon_rate, notional, k_price, critical_rate):
+    def jams_root_bond(self, option_expiry, payment_schedule, coupon_rate, N, k_price, critical_rate):
         bond_price = 0
 
         for i in range(len(payment_schedule)):
@@ -298,18 +297,20 @@ class hwPricer:
             bond_adj = self.model.bond_adj_factor(option_expiry, current_time)
             bond_price_i = bond_adj * np.exp(-bond_sens * critical_rate)
 
-            coupon_cashflow = notional * coupon_rate * accrual_period
+            coupon_cashflow = N * coupon_rate * accrual_period
             if i == len(payment_schedule) - 1:
-                coupon_cashflow += notional
+                coupon_cashflow += N
 
             bond_price += coupon_cashflow * bond_price_i
 
         return bond_price - k_price
 
-    def find_rstar_bond(self, option_expiry, payment_schedule, coupon_rate, notional, k_price, lower_bound=-5.0, upper_bound=5.0):
-        root_func = lambda rate: self.jams_root_bond(option_expiry, payment_schedule, coupon_rate, notional, k_price, rate)
+    def find_rstar_bond(self, option_expiry, payment_schedule, coupon_rate, N, k_price, lower_bound=-5.0, upper_bound=5.0):
+        root_func = lambda rate: self.jams_root_bond(option_expiry, payment_schedule, coupon_rate, N, k_price, rate)
         try:
             critical_rate = brentq(root_func, lower_bound, upper_bound, xtol=1e-12)
         except ValueError:
             critical_rate = brentq(root_func, -10.0, 10.0, xtol=1e-12)
         return critical_rate
+
+print("pricer works")
